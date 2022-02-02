99831bbbae67592572900e4f1d8297276e6b3176
jvwlehti <93128297+jvwlehti@users.noreply.github.com>
2022-02-02 11:30:55 +0200

    validointi lisätty

--- index.js
+++ index.js
@@ -79,21 +79,23 @@ const generateId = () => {
     return Math.floor(Math.random() * 1000)
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (body.name === undefined || body.number === undefined) {
+    /*if (body.name === undefined || body.number === undefined) {
         return response.status(400).json({ error: 'name missing or number missing' })
-    }
+    }*/
 
     const person = new Person({
         name: body.name,
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person
+        .save().then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -122,6 +124,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
